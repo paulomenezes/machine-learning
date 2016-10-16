9b23d8066e73c8a283a52e7ef535f8bd4c922e5c
Paulo Menezes <paulo.hgmenezes@gmail.com>
2016-10-16 13:11:22 -0300

    Area under the ROC curve

--- Grupo4/roc.m
+++ Grupo4/roc.m
@@ -36,7 +36,54 @@ function [a] = roc(s, y, N, P)
   
   plot(R(:, 1), R(:, 2));
   
-  area = trapz(R(:, 1), R(:, 2));
+  % area2 = trapz(R(:, 1), R(:, 2));
+  % area2
+  
+  area = area_roc(s, y, N, P);
   area
   
+endfunction
+
+function [A] = area_roc(s, y, N, P)
+  FP = 0;
+  FPprev = 0;
+  TPprev = 0;
+  TP = 0;
+  
+  m = vertcat(s, y);
+  m = m';
+  m = sortrows(m, 1);
+  
+  A = 0;
+  Fprev = -10000;
+  i = size(m, 1);
+  
+  while (i > 0)
+    if (m(i, 1) != Fprev)
+      A = A + trapezoid_area(FP, FPprev, TP, TPprev);
+      % R = vertcat(R, [ FP/N TP/P m(i, 1) ]);
+      Fprev = m(i, 1);
+      FPprev = FP;
+      TPprev = TP;
+    endif
+    if (m(i, 2) == 1)
+      TP = TP + 1;
+    else
+      FP = FP + 1;
+    endif
+    
+    i = i - 1;
+  endwhile
+  
+  A = A + trapezoid_area(N, FPprev, N, TPprev);
+  A = A / (P * N);
+  % R = vertcat(R, [ FP/N TP/P 0 ]);
+  
+endfunction
+
+function [A] = trapezoid_area(X1, X2, Y1, Y2)
+  base = abs(X1 - X2);
+  height = (Y1 + Y2) / 2;
+  
+  A = base * height;
 endfunction
